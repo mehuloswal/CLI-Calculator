e97dc6d61f9b45131185970031e44ae3443bca12
Mehul Oswal <mehuloswal21@gmail.com>
2022-03-10 00:10:33 +0530

    Parse in the order of the output fn

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ fn main() {
     let mut args: Args = args();
 
     let first = args.nth(1).unwrap();
-    let second = args.nth(0).unwrap();
     let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let second = args.nth(0).unwrap();
     let first_number = first.parse::<f32>().unwrap();
     let second_number = second.parse::<f32>().unwrap();
     let result = operate(operator, first_number, second_number);
@@ -13,6 +13,7 @@ fn main() {
         "{:?}",
         output(first_number, second_number, operator, result)
     );
+    // println!("{:?} {} {}", first, operator, second)
 }
 
 fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
@@ -20,8 +21,8 @@ fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
         '+' => first_number + second_number,
         '-' => first_number - second_number,
         '/' => first_number / second_number,
-        '*' => first_number * second_number,
-        _ => 0.0,
+        '*' | 'x' | 'X' => first_number * second_number,
+        _ => panic!("Invalid operator used."),
     }
 }
 
